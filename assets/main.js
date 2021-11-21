32edf9ff20facc276fa4e1111de7de350f4cc73f
Jimmy Yuan <lionelmessi6410@gmail.com>
2021-11-21 22:24:27 +0800

    Update index.html

--- assets/main.js
+++ assets/main.js
@@ -31,3 +31,11 @@ function showRecruit() {
   recruit = 1 - recruit;
 }
 
+function myFunction() {
+  var x = document.getElementById("home");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
